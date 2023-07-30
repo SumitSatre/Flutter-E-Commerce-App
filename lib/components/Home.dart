b87c62c6d7bb04d88d6d420c2a046d99e964827e
Sumit Satre <sumitsatre03@gmail.com>
2023-07-30 09:55:53 +0530

    30-07-2023

--- lib/components/Home.dart
+++ lib/components/Home.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget{
+
+class HomePage extends StatefulWidget{
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var CategoryData = {};
+
+  void fetchData () async {
+    let responseData = await http.get(
+      Uri("https://flutter-app-backend-qy7f.onrender.com/api/categories")
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +32,7 @@ class HomePage extends StatelessWidget{
           children: [
             Row(
               children: [
-                
+
               ],
             )
           ],
